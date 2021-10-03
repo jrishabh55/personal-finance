aa75c6c4d0b4b4acf6468f8af57185f1b158af83
Rishabh Jain <rishabh@codeation.io>
2021-10-04 00:56:20 +0530

    feat: file upload

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,28 +1,59 @@
+import { Alert } from '@mui/material';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
-import { FC } from 'react';
+import { Box } from '@mui/system';
+import { useAuthContext } from 'contexts/AuthContext';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { ChangeEventHandler, FC, useState } from 'react';
 
 const Input = styled('input')({
   display: 'none'
 });
 export const Dashboard: FC = () => {
+  const [status, setStatus] = useState<'uploading' | 'uploaded' | 'failed'>();
+  const [auth] = useAuthContext();
+
+  const handleClick: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setStatus('uploading');
+    if (target.files?.length) {
+      const file = target.files[0];
+      const storage = getStorage();
+      const storageRef = ref(storage, `statements/${auth.user?.uid}/${Date.now()}-${file.name}`);
+      // 'file' comes from the Blob or File API
+      uploadBytes(storageRef, file)
+        .then(() => {
+          console.log('Uploaded a blob or file!');
+          setStatus('uploaded');
+        })
+        .catch((error) => {
+          console.error(error);
+          setStatus('failed');
+        });
+    }
+  };
+
   return (
     <Grid
       container
       classes={{ root: 'w-screen h-screen' }}
       justifyContent="flex-end"
       alignItems="flex-start">
-      <label htmlFor="contained-button-file">
-        <Input
-          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-          id="contained-button-file"
-          type="file"
-        />
-        <Button variant="contained" component="span">
-          Upload
-        </Button>
-      </label>
+      <Box alignItems="flex-end" display="flex" flexDirection="column">
+        <label htmlFor="contained-button-file">
+          <Input
+            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+            id="contained-button-file"
+            type="file"
+            onChange={handleClick}
+          />
+          <Button variant="contained" component="span" disabled={!!status}>
+            {status === 'uploading' ? 'Uploading' : 'Upload'}
+          </Button>
+        </label>
+        {status === 'uploaded' && <Alert severity="success">File uploaded successfully!</Alert>}
+        {status !== 'failed' && <Alert severity="error">File uploaded successfully failed!</Alert>}
+      </Box>
     </Grid>
   );
 };
