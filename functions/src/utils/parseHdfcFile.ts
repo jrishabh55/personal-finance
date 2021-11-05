24530d065e50bff46ac2cad385c6f2ca05bc57d2
Rishabh Jain <rishabh@codeation.io>
2021-11-05 23:16:49 +0530

    feat: parseHdfcBank

--- functions/src/utils/parseHdfcFile.ts
+++ functions/src/utils/parseHdfcFile.ts
@@ -1,22 +1,31 @@
-import xlsx from "node-xlsx";
-import {StatementUpload} from "./types";
+import dayjs from 'dayjs';
+import xlsx from 'node-xlsx';
+import { StatementUpload } from './types';
 
 const parseHdfcFile = (
-    file: string | ArrayBuffer,
-    options?: Record<string, any>
+  file: string | ArrayBuffer,
+  options?: Record<string, any>,
 ): StatementUpload[] => {
   // parse xlsx file
   const [sheet]: any = xlsx.parse(file, options);
   sheet.data.splice(0, 22);
-  // return sheet.data;
 
   const data: Array<StatementUpload> = [];
   for (const row of sheet.data) {
-    if (row.length === 0 || row[0].includes("*")) {
+    if (row.length === 0 || row[0].includes('*')) {
       break;
     }
+
+    const [DD, MM, YY] = row[0].split('/').map(Number);
+
+    const date = dayjs(new Date(+`20${YY}`, MM, DD));
+
+    if (!date.isValid()) {
+      console.log('Invalid', row[0]);
+    }
+
     data.push({
-      date: row[0],
+      date: date,
       description: row[1],
       referenceId: row[2],
       amount: parseFloat(row[5] || row[4]),
