a2d8d33e7d2d4892ac376059e1093bde022c8fe0
Rishabh Jain <rishabh@codeation.io>
2021-11-15 15:58:03 +0530

    feat: dashboard component

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
+import Chart from 'charts';
+import { ChartData } from 'charts/types';
 import KPIRow from 'components/KPIRow';
 import { useAuthContext } from 'contexts/AuthContext';
 import numeral from 'numeral';
@@ -7,36 +8,41 @@ import { FC, useEffect, useState } from 'react';
 import { KPI } from 'types/global';
 import getAggregatedKpis from 'utils/getAggregatedKpis';
 
-const Input = styled('input')({
-  display: 'none'
-});
-
 export const Dashboard: FC = () => {
   const [kpis, setKpis] = useState<KPI[]>([]);
+  const [data, setData] = useState<ChartData>();
   const [auth] = useAuthContext();
 
   useEffect(() => {
     getAggregatedKpis({ uid: auth.user?.uid, accountId: 'uPXpnGOrD4k6UH3bYxYe' }).then((res) => {
       const kpi = res.data();
-      console.log('Calling', kpi);
+
       if (!kpi) {
         return;
       }
 
-      setKpis(
-        kpis.concat([
+      setKpis([
+        {
+          id: 'total-deposit',
+          value: numeral(kpi.totalDeposit).format('0.0a'),
+          title: 'Overall Deposit'
+        },
+        {
+          id: 'total-withdrawal',
+          value: numeral(kpi.totalWithdrawal).format('0.0a'),
+          title: 'Overall Expenditure'
+        }
+      ]);
+
+      setData({
+        labels: ['Withdrawal', 'Deposit'],
+        datasets: [
           {
-            id: 'total-deposit',
-            value: numeral(kpi.totalDeposit).format('0.0a'),
-            title: 'Overall Deposit'
-          },
-          {
-            id: 'total-withdrawal',
-            value: numeral(kpi.totalWithdrawal).format('0.0a'),
-            title: 'Overall Expenditure'
+            label: 'Overall Expenses',
+            data: [kpi.totalWithdrawal, kpi.totalDeposit]
           }
-        ])
-      );
+        ]
+      });
     });
   }, [auth.user]);
 
@@ -45,19 +51,10 @@ export const Dashboard: FC = () => {
       <Grid item xs="auto">
         <KPIRow kpis={kpis} />
       </Grid>
-
-      {/* <Grid item xs="auto">
-
-      </Grid> */}
       <Grid item xs={12}>
         <Grid container spacing={0}>
-          <Grid
-            item
-            xs={12}
-            justifyContent="flex-start"
-            alignItems="flex-start"
-            classes={{ root: 'border-white' }}>
-            <pre>{JSON.stringify(kpis, null, 2)}</pre>
+          <Grid item xs={3} justifyContent="flex-start" alignItems="flex-start">
+            {data && <Chart id="bar-chart" type="bar" data={data} />}
           </Grid>
         </Grid>
       </Grid>
